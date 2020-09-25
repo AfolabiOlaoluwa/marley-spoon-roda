a59bb6f17c230a40febd9dec686cd9069d86f2cb
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2020-09-25 07:51:35 +0100

    provided contentful model config for running test

--- spec/models/spec_helper.rb
+++ spec/models/spec_helper.rb
@@ -1,4 +1,12 @@
 ENV["RACK_ENV"] = "test"
 require_relative '../../models/recipe'
+require 'contentful_model'
 
 require_relative '../minitest_helper'
+
+ContentfulModel.configure do |config|
+  config.space = ''
+  config.access_token = ''
+  config.environment = "master"
+end
+
