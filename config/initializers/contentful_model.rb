972c61a3ba1e6b5fec564349f4c42d132d602e99
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2020-09-25 01:36:56 +0100

    set contenful api environment config as master

--- config/initializers/contentful_model.rb
+++ config/initializers/contentful_model.rb
@@ -1,4 +1,5 @@
 ContentfulModel.configure do |config|
   config.space = ENV.fetch('CONTENTFUL_SPACE_ID')
   config.access_token = ENV.fetch('CONTENTFUL_ACCESS_TOKEN')
+  config.environment = "master"
 end
