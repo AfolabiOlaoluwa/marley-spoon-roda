7ef636c25213c95e8444185571ad11bf8157faf4
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2020-09-28 20:24:45 +0100

    Cleaned up recipe.rb to increase application performance by freezing string literals

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,8 +1,8 @@
-require 'contentful_model'
+# frozen_string_literal: true
 
+require 'contentful_model'
 class Recipe < ContentfulModel::Base
   self.content_type_id = 'recipe'
-
   def self.all_recipes
     all.load!
   end
